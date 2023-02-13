23e76e67b81e746b19766c2d2157ca09a2de6491
shm_usuf <shmusuf@gmail.com>
2023-02-13 13:02:27 +0600

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log("Request is arrived");
+});
+
+server.listen(port, localhost, () => {
+  console.log("server is running...");
+});
